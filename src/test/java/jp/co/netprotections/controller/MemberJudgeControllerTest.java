d0f01d40596caac88490c280ee59316e55d6d564
Ryo Kasai <rkasai@netprotections.co.jp>
2020-06-10 22:43:29 +0900

    first commit

---
+++ src/test/java/jp/co/netprotections/controller/MemberJudgeControllerTest.java
@@ -0,0 +1,13 @@
+package jp.co.netprotections.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberJudgeControllerTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
