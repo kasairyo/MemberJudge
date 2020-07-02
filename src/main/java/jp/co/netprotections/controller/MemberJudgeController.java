29380e619f6c50610533e26905e62cc91f1df08c
Ryo Kasai <rkasai@netprotections.co.jp>
2020-07-03 00:22:22 +0900

    POST, GETメソッドが通る状態を作成

--- src/main/java/jp/co/netprotections/controller/MemberJudgeController.java
+++ src/main/java/jp/co/netprotections/controller/MemberJudgeController.java
@@ -1,15 +1,30 @@
 package jp.co.netprotections.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jp.co.netprotections.dto.MemberJudgeRequestDto;
+
 @RestController
 public class MemberJudgeController {
 	@RequestMapping(value = "/judge", method = {RequestMethod.POST})
-	public String judgeMember(@RequestBody String json) {
-		return json;
+	public Map<String, ArrayList<MemberJudgeRequestDto>> judgeMember(@RequestBody String candidates) {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, ArrayList<MemberJudgeRequestDto>> resultList = new HashMap<String, ArrayList<MemberJudgeRequestDto>>();
+		try { resultList = mapper.readValue(candidates, new TypeReference<Map<String, ArrayList<MemberJudgeRequestDto>>>() { });
+		} catch (IOException e) {
+		}
+		return resultList;
 	}
 	@RequestMapping(value = "/judge", method = {RequestMethod.GET})
 	public String test() {
